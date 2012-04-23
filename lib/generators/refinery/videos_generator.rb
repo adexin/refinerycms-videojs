407815b3fcd3fe8d3b3febf407cefdce9dd51bcd
Anton Mishchuk <antonmi@mail.ru>
2012-04-23 23:38:48 +0300

    init

--- lib/generators/refinery/videos_generator.rb
+++ lib/generators/refinery/videos_generator.rb
@@ -3,7 +3,7 @@ module Refinery
     source_root File.expand_path('../videos/templates', __FILE__)
 
     def rake_db
-      rake("refinery_videos:install:migrations")
+      rake("refinery_videojs:install:migrations")
     end
 
     def generate_videos_initializer
